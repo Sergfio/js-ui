87a33b1ebed21533fbc00f6566469b31f1e70a47
Stefan Focht <koechinnen.maechte0t@icloud.com>
2024-07-02 01:02:56 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    (function() {
+        var fastSin = function(steps) {
+            var table = [],
+            ang = 0,
+            angStep = (Math.PI * 2) / steps;
+            do {
+                table.push(Math.sin(ang));
+                ang += angStep;
+            } while (ang < Math.PI * 2);
+            return table;
+        }
+    })
+})
